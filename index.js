1000cf688dd827ec841e740ef3693761ee273063
Joshua Dogma <jdogma94@gmail.com>
2023-10-31 15:59:38 -0400

    Pseudo-code done. No code written at this point

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log(`Hello Chump`);
+// prompt a user to enter a list of comma-separated froyo flavors
+// store the result in an array named flavors 
+//create a object to keep count of how many orders there are of each flavor
+// create a loop to go through the array and add to object 
+//create a function that will keep the object updated  

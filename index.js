9f3af3d728f926616dd055dfd8dcbf78c35d7bac
Joshua Dogma <jdogma94@gmail.com>
2023-10-31 16:58:04 -0400

    User prompt added to array with split. Object intialized but empty.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-// prompt a user to enter a list of comma-separated froyo flavors
-// store the result in an array named flavors 
+
+
 //create a object to keep count of how many orders there are of each flavor
 // create a loop to go through the array and add to object 
-//create a function that will keep the object updated  
+//create a function that will keep the object updated
+
+const UserInput = prompt(`Enter flavors:`);       // prompt a user to enter a list of comma-separated froyo flavors
+
+const flavors = UserInput.split(',');             // store the result in an array named flavors 
+
+const orderHistory = {
+  flavors : []
+};
+
+

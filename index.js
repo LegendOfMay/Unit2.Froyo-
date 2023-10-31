99f79c8d76cc9697420ed08a495385ee673d3f46
Joshua Dogma <jdogma94@gmail.com>
2023-10-31 19:09:55 -0400

    Created a loop that goes thru the array and add flavor if seen multiple times. Have not wrapped it in a function yet.

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 
 
-//create a object to keep count of how many orders there are of each flavor
-// create a loop to go through the array and add to object 
+
+
 //create a function that will keep the object updated
 
 const UserInput = prompt(`Enter flavors:`);       // prompt a user to enter a list of comma-separated froyo flavors
 
 const flavors = UserInput.split(',');             // store the result in an array named flavors 
 
-const orderHistory = {
-  flavors : []
+const orderHistory = {};                          //create a object to keep count of how many orders there are of each flavor
+
+for(i = 0; i <flavors.length; i++){               // create a loop to go through the array and add to object 
+  const flavor = flavors[i];
+  if(orderHistory[flavor]) {
+    orderHistory[flavor] += 1;
+  }else {
+    orderHistory[flavor] = 1;
+  };
 };
+
+
+
+console.log(orderHistory);
+
+
 
 

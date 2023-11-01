5463f875e83155a4c2e4185ffa9f52e48ca687e3
Joshua Dogma <jdogma94@gmail.com>
2023-11-01 10:41:32 -0400

    Ran out of time, have to submit . Could not figure out the functions part

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+// Could not figure out how to add a function  to the loop, bigger picture is unclear for me. 
 
 
 //create a function that will keep the object updated
@@ -9,7 +9,7 @@ const UserInput = prompt(`Enter flavors:`);       // prompt a user to enter a li
 const flavors = UserInput.split(',');             // store the result in an array named flavors 
 
 const orderHistory = {};                          //create a object to keep count of how many orders there are of each flavor
-
+ 
 for(i = 0; i <flavors.length; i++){               // create a loop to go through the array and add to object 
   const flavor = flavors[i];
   if(orderHistory[flavor]) {
@@ -18,11 +18,7 @@ for(i = 0; i <flavors.length; i++){               // create a loop to go through
     orderHistory[flavor] = 1;
   };
 };
-
-
-
-console.log(orderHistory);
-
+return;
 
 
 
